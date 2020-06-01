13c227ac92409397de63a186a5bc0ca541bb4830
“maruifeng” <maruifeng@dxy.cn>
2020-06-01 16:49:43 +0800

    first commit

---
+++ src/main/java/org/merry/code/netty/echo/EchoServerHandler.java
@@ -0,0 +1,54 @@
+package org.merry.code.netty.echo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+
+/**
+ * @author rfma
+ * @description
+ * @date 2020/6/1
+ */
+@ChannelHandler.Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    /**
+     * 每个信息入站都会调用
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("Server received:" + in.toString(CharsetUtil.UTF_8));
+        ctx.write(in);
+    }
+
+    /**
+     * 通知处理器最后的 channelread() 是当前批处理中的最后一条消息时调用
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    /**
+     * 读操作时捕获到异常时调用
+     * @param ctx
+     * @param cause
+     * @throws Exception
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
