5b9ff3c763ef35a145e063ef2fcb57b125922be5
“maruifeng” <maruifeng@dxy.cn>
2020-06-01 17:27:48 +0800

    echo server

--- src/main/java/org/merry/code/netty/echo/EchoServer.java
+++ src/main/java/org/merry/code/netty/echo/EchoServer.java
@@ -1,10 +1,62 @@
 package org.merry.code.netty.echo;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
+
 /**
  * @author rfma
  * @description echo
  * @date 2020/6/1
  */
 public class EchoServer {
+    private final int port;
 
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        if (args.length != 1) {
+            System.err.println("Usage:" + EchoServer.class.getSimpleName() + "<port>");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+        new EchoServer(port).start();
+    }
+
+    public void start() throws InterruptedException {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(group)
+                .channel(NioServerSocketChannel.class)
+                .localAddress(new InetSocketAddress(port))
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        socketChannel.pipeline().addLast(
+                                new EchoServerHandler()
+                        );
+                    }
+                });
+
+            ChannelFuture f = b.bind().sync();
+            System.out.println(EchoServer.class.getName() + " started and listen on " +
+                    f.channel().localAddress());
+            f.channel().close().sync();
+
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+
+
+    }
 }
